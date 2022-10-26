50da2a1edd844aee0f91d95f745e1493e6984a6d
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2022-10-25 22:31:10 -0300

    Corrigindo o grafico

--- src/views/dashboard/Default/chart-data/total-growth-bar-chart.js
+++ src/views/dashboard/Default/chart-data/total-growth-bar-chart.js
@@ -1,87 +1,95 @@
-// ===========================|| DASHBOARD - TOTAL GROWTH BAR CHART ||=========================== //
+import { render } from "sass";
 
-const chartData = {
-    height: 480,
-    type: 'bar',
-    options: {
-        chart: {
-            id: 'bar-chart',
-            stacked: true,
-            toolbar: {
-                show: true
+// ===========================|| DASHBOARD - TOTAL GROWTH BAR CHART ||=========================== //
+const categorias = ['Proteina', 'Carboidrato', 'Gordura sat', 'Calcio', 'Energia', 'VitaminaC', 'Cálcio', 'Ferro'];
+
+const valor1 = JSON.parse(localStorage.getItem('consumido'));
+console.log(valor1);
+
+class chartData{
+    const teste = {
+        height: 480,
+        type: 'bar',
+        options: {
+            chart: {
+                id: 'bar-chart',
+                stacked: false,
+                toolbar: {
+                    show: true
+                },
+                zoom: {
+                    enabled: true
+                }
             },
-            zoom: {
-                enabled: true
-            }
-        },
-        responsive: [
-            {
-                breakpoint: 480,
-                options: {
-                    legend: {
-                        position: 'bottom',
-                        offsetX: -10,
-                        offsetY: 0
+            responsive: [
+                {
+                    breakpoint: 480,
+                    options: {
+                        legend: {
+                            position: 'bottom',
+                            offsetX: -10,
+                            offsetY: 0
+                        }
                     }
                 }
-            }
-        ],
-        plotOptions: {
-            bar: {
-                horizontal: false,
-                columnWidth: '50%'
-            }
-        },
-        xaxis: {
-            type: 'category',
-            categories: ['Proteina', 'Carboidrato', 'Gordura sat', 'Calcio', 'Ferro', 'Açucar', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        legend: {
-            show: true,
-            fontSize: '14px',
-            fontFamily: `'Roboto', sans-serif`,
-            position: 'bottom',
-            offsetX: 20,
-            labels: {
-                useSeriesColors: false
+            ],
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '50%',
+                    dataLabels: {
+                        position: 'top'
+                    }
+                }
             },
-            markers: {
-                width: 16,
-                height: 16,
-                radius: 5
+            xaxis: {
+                type: 'category',
+                categories: categorias
             },
-            itemMargin: {
-                horizontal: 15,
-                vertical: 8
+            legend: {
+                show: true,
+                fontSize: '14px',
+                fontFamily: `'Roboto', sans-serif`,
+                position: 'bottom',
+                offsetX: 20,
+                labels: {
+                    useSeriesColors: false
+                },
+                markers: {
+                    width: 16,
+                    height: 16,
+                    radius: 5
+                },
+                itemMargin: {
+                    horizontal: 15,
+                    vertical: 8
+                }
+            },
+            fill: {
+                type: 'solid'
+            },
+            dataLabels: {
+                enabled: false
+            },
+            grid: {
+                show: true
             }
         },
-        fill: {
-            type: 'solid'
-        },
-        dataLabels: {
-            enabled: false
-        },
-        grid: {
-            show: true
-        }
-    },
-    series: [
-        {
-            name: 'Investment',
-            data: [35, 125, 35, 35, 35, 80, 35, 20, 35, 45, 15, 75]
-        },
-        {
-            name: 'Loss',
-            data: [35, 15, 15, 35, 65, 40, 80, 25, 15, 85, 25, 75]
-        },
-        {
-            name: 'Profit',
-            data: [35, 145, 35, 35, 20, 105, 100, 10, 65, 45, 30, 10]
-        },
-        {
-            name: 'Maintenance',
-            data: [0, 0, 75, 0, 0, 115, 0, 0, 0, 0, 150, 0]
-        }
-    ]
+
+        series: [
+            {
+                name: 'Sugerido',
+                data: [34, 125, 35, 35, 35, 80, 35, 20],
+                goals: [10, 10, 101, 10]
+            },
+            {
+                name: 'Ingerido',
+                data: [35, 15, 15, 35, 65, 40, 80, 25]
+            }
+        ]
+    };
+    render(){
+        return teste;
+    }
 };
 export default chartData;
